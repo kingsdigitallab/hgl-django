dad7410734d44721e449c5953c6bff08d17997a4
Neil Jakeman <neil.jakeman@kcl.ac.uk>
2015-11-11 16:02:55 +0000

    Still working on getting basic settings up and running

---
+++ .vagrant_provisioning/local_settings.py
@@ -0,0 +1,20 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.fb.backends.mysql',
+        'NAME': 'django_irt_geo',
+        'USER': 'app_irt_data_rw',
+        'PASSWORD': 'u6$5HKy7EytjuHGy',
+        'HOST': 'db-data.inslib.cch.kcl.ac.uk',
+        'PORT': '',
+    },
+}
+
+INTERNAL_IPS = ('0.0.0.0', '127.0.0.1', '::1')
+
+SECRET_KEY = '12345'
+
+FABRIC_USER = 'njakeman'
+
+CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_HTTPONLY = False
+SESSION_COOKIE_SECURE = False
