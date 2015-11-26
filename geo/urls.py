91b904360789122410baf5387de9d837088eea68
root <root@vmbase-debian-7.cch.kcl.ac.uk>
2015-11-26 16:10:29 +0000

    Convex hull dev commits

--- geo/urls.py
+++ geo/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-    (r'^kml', 'geo.views.kml')
+    (r'^kml', 'geo.views.kml'),
+    (r'^convex-hull/$','geo.views.convex_hull'),
 )
