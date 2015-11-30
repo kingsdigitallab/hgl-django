31abbcdcc8c4edda44cbe9cf4f3d9207640500db
Ginestra Ferraro <ginestra.ferraro@kcl.ac.uk>
2015-11-30 16:24:26 +0000

    Adding template options and styles for search pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 /* UI and Foundation stuff here */
+
+$(document).ready(function() {
+
+	// Expand / Collapse
+
+	$('.expander').bind("click", function() {
+		$(this).next('.collapsible').slideToggle(400).removeClass("hide");
+		$("i", this).toggleClass("fa-caret-down fa-caret-right");
+		return false;
+	});
+
+});
