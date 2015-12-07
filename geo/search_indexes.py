be3e20edcaf253c9ed4fa618b8f60b4de9bf7f4b
Neil Jakeman <njakeman@vmbase-debian-7.cch.kcl.ac.uk>
2015-12-07 14:42:41 +0000

    No change

--- geo/search_indexes.py
+++ geo/search_indexes.py
@@ -5,9 +5,13 @@ class LocusIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     #location_feature_type = indexes.MultiValueField(faceted=True)
     name =  indexes.CharField(model_attr='name')
+    period = indexes.MultiValueField(faceted=True)
 
     #def prepare_location_feature_type(self, object):
     #    return [ ft.description for ft in object.featuretype_fk.all() ]
+
+#    def prepare_period(self, obj):
+        
 
     def get_model(self):
         return Locus
