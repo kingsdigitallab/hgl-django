14575f242e20eae766e11cf18381475f37163c01
njakeman <neil_jakeman@hotmail.com>
2017-09-27 15:14:29 +0100

    Change null geoms

--- geo/pelagiosUrls.py
+++ geo/pelagiosUrls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-    (r'^downloads/json/dump.js','geo.pelagiosViews.json_dump'),
-    (r'^downloads/json/(\d+).js','geo.pelagiosViews.individual_json_dump'),
+    (r'^downloads/json/dump.json','geo.pelagiosViews.json_dump'),
+    (r'^downloads/json/(\d+).json','geo.pelagiosViews.individual_json_dump'),
 )
