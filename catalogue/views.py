03d8f6511e9502acc7e7dc9ace2c48229338067d
njakeman <neil_jakeman@hotmail.com>
2019-02-27 11:54:20 +0000

    Catalogue search

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from catalogue.models import *
 
-# Create your views here.
+from django.template import RequestContext
+from django.shortcuts import get_object_or_404, render_to_response, HttpResponse, render,HttpResponseRedirect
+
+    
+def recordview(request):
+    id = request.GET.get('id','')
+    item = BasicArchiveModel.objects.get(pk=id)    
+    context = {}
+    context['record'] = item
+    return render_to_response('../templates/single-cat-record.html',context,context_instance=RequestContext(request))
