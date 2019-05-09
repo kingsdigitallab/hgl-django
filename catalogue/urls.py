9ea9a5c2e45c42ab4cc1c7963088a6430cc71520
njakeman <neil_jakeman@hotmail.com>
2019-05-09 12:24:49 +0100

    Hypothesis search by group

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     (r'^recordview/$','catalogue.views.recordview'),
+    (r'^hypothesis-tag/(\d+)', 'catalogue.views.tag_search'),
     (r'^(\d+)/', 'catalogue.views.browse_item'),
     (r'^$', 'catalogue.views.browse'),
 )
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,4 +1,6 @@
 from catalogue.models import *
+from requests_oauthlib import OAuth2Session
+import simplejson
 
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response, HttpResponse, render,HttpResponseRedirect
@@ -22,3 +24,13 @@ def browse_item(request, id):
     context = {}
     context['cat'] = BasicArchiveModel.objects.get(pk=id)
     return render_to_response('../templates/browse_item.html',context,context_instance=RequestContext(request))
+
+def tag_search(request, id):
+    context = {}
+    searchurl = 'https://hypothes.is/api/search?tag=HGL:' + str(id)
+    print searchurl
+    token = {'access_token': '6879-AHM93XsH2RBSu71vTSt6odLIFRPt35oFbmXYqpSn-5E', 'token_type': 'Bearer'}
+    hyp = OAuth2Session(token=token)
+    ret = hyp.get(searchurl).json()
+    return HttpResponse(simplejson.dumps(ret),content_type='application/json')
+
