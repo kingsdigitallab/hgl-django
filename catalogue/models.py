0219782f1a04e594d9a9415fcdfc49e01f735ec0
njakeman <neil_jakeman@hotmail.com>
2020-02-11 11:55:40 +0000

    Add Repositories admin and show all former references on Cat item page

--- catalogue/models.py
+++ catalogue/models.py
@@ -90,6 +90,9 @@ class Repository(models.Model):
     desc = models.CharField(max_length=100)
     def __unicode__(self):
         return self.desc
+
+    class Meta:
+        verbose_name_plural = "Repositories"
 		
 class Person(models.Model):
     surname = models.CharField(max_length=50)
