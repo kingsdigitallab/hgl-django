0ddd3b30f78dfb012cd424df72026417edac5d22
fieldWorks-3D <info@field.works>
2021-01-26 15:35:48 +0000

    Fix map

--- assets/js/map.js
+++ assets/js/map.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
         zoom: 5
     });
     // and add tiles
-    var awmc = new L.TileLayer("http://{s}.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png",{subdomains:'abcd',
-     attribution:'<a href="http://awmc.unc.edu/wordpress/">AWMC</a>'}).addTo(map)
+    //var awmc = new L.TileLayer("http://{s}.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png",{subdomains:'abcd',
+    // attribution:'<a href="http://awmc.unc.edu/wordpress/">AWMC</a>'}).addTo(map)
 
 
     var ggl = new L.Google('HYBRID');
 
-    var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 
     var switcher = new L.Control.Layers({'AWMC':awmc, 'Google':ggl, 'Open Street Map':osm },{}).addTo(map) 
