a729f80a2a5f4114580514433263d0c3400edde6
Elliott Hall <elliott.hall@kcl.ac.uk>
2024-03-27 14:07:42 +0000

    remove debug

--- hgl/settings/production.py
+++ hgl/settings/production.py
@@ -8,7 +8,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=['hgl-os.kdl.kcl.ac.uk'])
 INTERNAL_IPS = ['0.0.0.0', '127.0.0.1', '::1', '10.0.2.2']
-DEBUG=True
+
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
